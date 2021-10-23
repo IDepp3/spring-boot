0b2adb5d8abd0e79feb5bb515ff9e2b535eadfcd
IDepp3 <alvarez.joaquinez@gmail.com>
2021-10-23 14:04:14 -0300

    Practica paginacion

---
+++ pagination/countries-pagination/src/main/java/com/depp3/countriespagination/CountriesPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.depp3.countriespagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesPaginationApplication.class, args);
+	}
+
+}
---
+++ pagination/countries-pagination/src/test/java/com/depp3/countriespagination/CountriesPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depp3.countriespagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
